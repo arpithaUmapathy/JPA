a466baa3d742ce82949b7d85e18190afcd95f377
apppz <arpithacu.xworkz@gmail.com>
2022-08-08 16:58:21 +0530

    Hibernate

---
+++ resort/src/com/xworkz/resort/DAO/ResortDAO.java
@@ -0,0 +1,7 @@
+package com.xworkz.resort.DAO;
+
+public interface ResortDAO  {
+	
+	boolean save(ResortDAO resortDTO);
+
+}
---
+++ resort/src/com/xworkz/resort/DAO/ResortDAOImpl.java
@@ -0,0 +1,11 @@
+package com.xworkz.resort.DAO;
+
+public class ResortDAOImpl implements ResortDAO{
+
+	@Override
+	public boolean save(ResortDAO resortDTO) {
+		
+		return false;
+	}
+
+}
